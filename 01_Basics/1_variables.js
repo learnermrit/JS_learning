e82c89d6efd0020a83983f2481e9d365c9e1d865
Mrityunjay Kumar <97528624+learnermrit@users.noreply.github.com>
2023-12-02 07:50:48 +0000

    datatyoes added

---
+++ 01_Basics/1_variables.js
@@ -0,0 +1,12 @@
+const id = 12456
+let email= "mrityunjay"  
+
+var password = "JK456" // not preffered as problem in scope and /function scope
+city= "Muzaffarpur"; // Allowed
+let state;  // undefined by default
+
+// id = 156  not allowed to change value of const
+
+
+console.log(id, email)  // comma can also be used
+console.table([id,email,password,city,state]) // another method to print many values instead of writing console.log multiple times
---
+++ 01_Basics/2_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version 
+
+// For Documentation 
+// original tC39.es/ecma262/#sec-intro
+// mdm
+
+//number => 2 to power 53
+// Bigint 
+//string => ""
+// boolean => true/False
+//null => standalone value
+// undifined => value is not assinged
+//symbol => specially used in React to finnd unique value
+
+
+// object
+
+let age = 56
+
+console.log(typeof(age));  // number
+console.log(typeof undefined); // undefined
+console.log(typeof null);   // type object
+
